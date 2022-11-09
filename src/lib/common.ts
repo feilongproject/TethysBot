dcd32b452095f461c68c454d55309bd055e390d2
feilongproject <flzs6137@gmail.com>
2022-11-09 23:26:52 +0800

    refactor: 从`plugins/identity.ts`移动`timeConver`函数到`lib/common.ts`

--- src/lib/common.ts
+++ src/lib/common.ts
@@ -24,3 +24,14 @@ export function debugAdmin(uid: string): boolean {
     }
     return true;
 }
+
+export function timeConver(timestamp: number | string | Date) {
+    const date = new Date(timestamp);
+    const year = date.getFullYear().toString().padStart(4, "0");
+    const month = date.getMonth().toString().padStart(2, "0");
+    const day = date.getDate().toString().padStart(2, "0");
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+    const seconds = date.getSeconds().toString().padStart(2, "0");
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
--- src/plugins/identity.ts
+++ src/plugins/identity.ts
@@ -1,3 +1,4 @@
+import { timeConver } from "../lib/common";
 import { IMessageEx } from "../lib/IMessageEx";
 
 export async function identityList(msg: IMessageEx) {
@@ -63,15 +64,4 @@ export async function warnWithReason(msg: IMessageEx) {
         log.error(err);
     });
 
-}
-
-function timeConver(timestamp: number | string | Date) {
-    const date = new Date(timestamp);
-    const year = date.getFullYear().toString().padStart(4, "0");
-    const month = date.getMonth().toString().padStart(2, "0");
-    const day = date.getDate().toString().padStart(2, "0");
-    const hours = date.getHours().toString().padStart(2, "0");
-    const minutes = date.getMinutes().toString().padStart(2, "0");
-    const seconds = date.getSeconds().toString().padStart(2, "0");
-    return `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
 }
